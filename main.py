be06b476f9b49bbc482de4e68d450f0f2093894f
shimanakahibiki <75057530+shimanakahibiki@users.noreply.github.com>
2020-12-16 14:28:56 +0900

    追加、削除、表示の処理、htmlを作りました。（削除の処理はできていない）

--- main.py
+++ main.py
@@ -4,22 +4,108 @@ import json
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 
+
 # カレンダー表示
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
+# スケジュールの表示
+@app.route('/schedule')
+def schedule():
+    return render_template("schedule.html")
+
+
+@app.route('/schedule/json', methods=["GET"])
+def schedule_get():
+    with open('schedule.json') as f:
+        json_data = json.load(f)
+
+    return jsonify(json_data)
+
+
 # 予定の追加
-@app.route('/change', methods=["POST"])
+@app.route('/change/add', methods=["POST"])
 def schedule_add():
-    pass
+    date = request.json.get('date', None)
+    schedule = request.json.get('schedule', None)
+
+    # print(date)
+    # print(schedule)
+
+    json_sc = {
+        "date": date,
+        "schedule": schedule
+    }
+
+    # 値が入ってなかった場合の処理
+    if not schedule or not date:
+        return jsonify({
+            "message": "Error"
+    })
+
+    # JSON読み込み
+    with open('schedule.json') as f:
+        sc_data = json.load(f)
+    # list変換
+    sc_list = list(sc_data)
+    # 入力されたデータを追加
+    sc_list.append(json_sc)
+    # 追加されたlistを書き込み
+    with open('schedule.json', 'w') as f:
+        json.dump(sc_list, f, indent=4, ensure_ascii=False)
+
+    return jsonify({
+        "message": "Success"
+    })
+
 
 # 予定の削除
-@app.route('/change', methods=[])
+@app.route('/change/del', methods=["POST"])
 def schedule_del():
-    pass
+    # 削除したい予定を持ってくる
+    date = request.json.get('date', None)
+    schedule = request.json.get('schedule', None)
 
+    # チェック
+    check = {
+        "date": date,
+        "schedule": schedule
+    }
 
+    # JSON読み込み
+    with open('schedule.json') as f:
+        sc_data = json.load(f)
+
+    # リスト型に変換
+    sc_list = list(sc_data)
+
+    # フラグ
+    flag = False
+
+    # 認証
+    for i in range(len(sc_list)):
+        print(sc_list[i], check)
+        if sc_list[i].get("date") == check["date"] and sc_list[i].get("schedule") == check["schedule"]:
+            flag = True
+            # 元データを削除
+            sc_list.remove(sc_list[i])
+
+            # ファイル書き込み
+            with open('schedule.json', 'w') as f:
+                json.dump(sc_list, f, indent=4, ensure_ascii=False)
+        else:
+            continue
+
+    if not flag:
+        return jsonify({
+            "message": "Error"
+        })
+
+    return jsonify({
+        "message": "Success"
+    })
 
 
 if __name__ == "__main__":
--- static/changeSchedule.js
+++ static/changeSchedule.js
@@ -1,1 +1,77 @@
 // カレンダーで選択した日付の予定を表示する処理、jsonファイルにデータを追加・削除する処理
+
+// 表示
+function init() {
+    // データの初期表示
+    fetch("/schedule/json").then(response => {
+        console.log(response)
+        response.json().then((data) => {
+            console.log(data) // 取得されたレスポンスデータをデバッグ表示
+            // データを表示させる
+            const tableBody = document.querySelector("#sc_list > tbody")
+            tableBody.innerHTML = ""
+
+            data.forEach(elm => {
+                // 1行づつ処理を行う
+                let tr = document.createElement('tr')
+                // 日付
+                let td = document.createElement('td')
+                td.innerText = elm.date
+                tr.appendChild(td)
+                // スケジュール
+                td = document.createElement('td')
+                td.innerText = elm.schedule
+                tr.appendChild(td)
+                //　削除ボタン
+                td = document.createElement('td')
+                td.innerHTML = "<input type='submit' id='sc_delete' value='削除' class='btn btn-danger'>"
+                tr.appendChild(td)
+
+                // 1行分をtableタグ内のtbodyへ追加する
+                tableBody.appendChild(tr)
+            })
+        })
+    })
+}
+
+init()
+// 追加
+document.getElementById("add-submit").addEventListener('click', (e) => {
+    // ボタンイベントのキャンセル
+    e.preventDefault()
+    console.log(document.getElementById('add_sc').value)
+
+    const obj = {date: document.getElementById("add_date").value, schedule: document.getElementById("add_sc").value};
+    const method = "POST";
+    const body = JSON.stringify(obj);
+    console.log(obj)
+
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+
+    fetch('/change/add', { method, headers, body }).then(function (response) {
+    init()
+    document.getElementById('message-container').innerHTML = "スケジュール登録完了"
+    document.getElementById('message-container').style.display = "block"
+    })
+  });
+
+// 削除
+document.getElementById("list_body").addEventListener('click', (e) => {
+    // ボタンイベントのキャンセル
+    e.preventDefault()
+
+
+    const method = "POST";
+
+
+
+
+    fetch('/change/del', { method }).then(function (response) {
+    init()
+    document.getElementById('message-container').innerHTML = "削除完了"
+    document.getElementById('message-container').style.display = "block"
+    })
+  });
