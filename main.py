10f5fae327d66e04b1e232a1ea57efb08417880d
k19110kk <ykidaruma29@gmail.com>
2020-12-17 16:15:21 +0900

    カレンダーに表示する予定の内部的なものはできました。

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, jsonify
 import json
+import datetime
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
@@ -8,6 +9,67 @@ app.config["JSON_AS_ASCII"] = False
 @app.route('/')
 def index():
     return render_template("index.html")
+
+# index.htmlに表示するsucedule.jsonの取得
+@app.route('/json', methods=["GET"])
+def calender_schedule_get():
+    with open('schedule.json') as f:
+        json_data = json.load(f)        
+
+    # 並び替え(日付が古い順)
+    json_data = sorted(json_data, key=lambda x:x['date'])  
+
+    # 現在の日付を取得
+    dt_now = datetime.datetime.now()    
+
+
+    # 現在の月より古いデータを削除    
+    dict1 = []
+    for j in json_data:  
+        date = j["date"][0:4] + j["date"][5:7]              
+        if not(int(date) < int(dt_now.strftime('%Y%m'))):            
+            dict1.append(j)   
+
+    # jsonファイルを更新する
+    with open('schedule.json', 'w') as f:
+        json.dump(dict1, f, indent=4, ensure_ascii=False)
+    
+    # 5文字より大きいの予定を省略する
+    limnum = 5
+    i = 0
+    for j in dict1:        
+        if len(j['schedule']) > limnum:
+            dict1[i]['schedule'] = j['schedule'][0:limnum] + "..."
+
+        i = i + 1            
+
+    # 同じ日付の予定が3つ以上ある場合、省略する（未完成）
+    i = 0
+    count = 0    
+    k = 0
+    dict2 = []    
+    for j in dict1:
+        # 最初
+        if i == 0:
+            tmp_date = j['date']            
+            dict2.append(j)  
+
+        #次から
+        else:
+            # 前のデータの日付と同じなら
+            if j['date'] == tmp_date:
+                count = count + 1
+                
+            # 前のデータの日付と違うなら
+            else:
+                count = 0
+
+            dict2.append(j)  
+                
+        i = i + 1                   
+       
+    #print(dict2)
+    return jsonify(dict2)
 
 # 予定の追加
 @app.route('/change', methods=["POST"])
--- static/calender.js
+++ static/calender.js
@@ -1,1 +1,36 @@
 // 月のカレンダーを表示
+
+// 表示
+function init() {
+    // データの初期表示
+    fetch("/json").then(response => {
+        console.log(response)
+        response.json().then((data) => {
+            console.log(data) // 取得されたレスポンスデータをデバッグ表示
+            // カレンダーを表示させるように変更してください
+            const tableBody = document.querySelector("#sc_list > tbody")
+            tableBody.innerHTML = ""            
+            data.forEach(elm => {                   
+
+                // 1行づつ処理を行う
+                let tr = document.createElement('tr')
+                // 日付
+                let td = document.createElement('td')
+                
+                td.innerText = elm.date
+                tr.appendChild(td)
+                // スケジュール
+                let td = document.createElement('td')                                  
+
+                td.innerText = elm.schedule
+                tr.appendChild(td)                
+
+                // 1行分をtableタグ内のtbodyへ追加する
+                tableBody.appendChild(tr)
+            })
+            // ここまで
+        })
+    })
+}
+
+init()
